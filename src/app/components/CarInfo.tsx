69b2090c4f5a0a93acb38c6a01a450790cb14569
Oleguito <tenzoriator@rambler.ru>
2024-03-11 18:25:29 +0300

    секси таблица на странице машины

--- src/app/components/CarInfo.tsx
+++ src/app/components/CarInfo.tsx
@@ -8,14 +8,30 @@ interface Props {
 const CarInfo = (props: Props) => {
     const car: Car = props.car;
     return (
-        <>
-            <p>id: {car.id}</p>
-            <p>Make: {car.make}</p>
-            <p>Model: {car.model}</p>
-            <p>Year: {car.year}</p>
-            <p>Mileage: {car.mileage}</p>
-            <p>Price: {car.price}</p>
-        </>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            <table className="min-w-full">
+                <thead className="bg-gray-800 text-white">
+                    <tr>
+                        <th className="px-4 py-2">ID</th>
+                        <th className="px-4 py-2">Make</th>
+                        <th className="px-4 py-2">Model</th>
+                        <th className="px-4 py-2">Year</th>
+                        <th className="px-4 py-2">Mileage</th>
+                        <th className="px-4 py-2">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="bg-gray-100">
+                        <td className="px-4 py-2">{car.id}</td>
+                        <td className="px-4 py-2">{car.make}</td>
+                        <td className="px-4 py-2">{car.model}</td>
+                        <td className="px-4 py-2">{car.year}</td>
+                        <td className="px-4 py-2">{car.mileage}</td>
+                        <td className="px-4 py-2">{car.price}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
 };
 
