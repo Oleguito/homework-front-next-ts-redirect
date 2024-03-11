a5acec2a1165ca6fe4818c58b1a1d17b27625f59
Oleguito <tenzoriator@rambler.ru>
2024-03-11 18:22:44 +0300

    см. ниже

--- src/app/components/CarInfo.tsx
+++ src/app/components/CarInfo.tsx
@@ -6,7 +6,17 @@ interface Props {
 }
 
 const CarInfo = (props: Props) => {
-    return <div>CarInfo</div>;
+    const car: Car = props.car;
+    return (
+        <>
+            <p>id: {car.id}</p>
+            <p>Make: {car.make}</p>
+            <p>Model: {car.model}</p>
+            <p>Year: {car.year}</p>
+            <p>Mileage: {car.mileage}</p>
+            <p>Price: {car.price}</p>
+        </>
+    );
 };
 
 export default CarInfo;
